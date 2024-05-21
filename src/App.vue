aeaedbbf2861d8b066d263e037d5d66f9b4bc7a4
gitrnamehere <105529595+gitnamehere@users.noreply.github.com>
2024-05-21 10:16:58 -0400

    add modal wrapper and cool opening animation

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import ModalWrapper from '@/components/ModalWrapper.vue'
+
+const modalOpen = ref(false)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="body">
+    <h1>Recursive Vue Modal Testing</h1>
+    <button @click="modalOpen = !modalOpen">Launch Modal</button>
+  </div>
+  <ModalWrapper :modalOpen="modalOpen" />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style scoped lang="scss">
+.fade-e .body {
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ src/components/ModalWrapper.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import TestModal from '@/components/TestModal.vue'
+
+interface Props {
+  modalOpen: boolean
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <Transition>
+    <TestModal v-if="modalOpen" />
+  </Transition>
+</template>
+
+<style>
+.v-enter-active {
+  transition:
+    width 0.35s ease,
+    opacity 0.35s ease,
+    height 0.5s 0.25s ease;
+}
+
+.v-leave-active {
+  transition:
+    width 0.35s 0.5s ease,
+    opacity 0.35s 0.5s ease,
+    height 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+  height: 32px;
+  width: 32px;
+}
+</style>
